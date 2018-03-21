14d4370f084cbbc5d2e08c28c0f2fd7661858340
brian-lacdoo <brian.lacdoo@hotmail.com>
2018-03-20 23:43:18 -0400

    Added a second println.

--- src/PollViewer.java
+++ src/PollViewer.java
@@ -1,5 +1,6 @@
 public class PollViewer {
     public static void main(String args[]){
         System.out.println("Hello World");
+        System.out.println("David is a beautiful person.");
     }
 }
