7626b54e4a95a89bd4b608016e3a4af71b3ea442
brian-lacdoo <brian.lacdoo@hotmail.com>
2018-03-22 23:09:48 -0400

    Second (test) commit of the Student class.

--- src/Student.java
+++ src/Student.java
@@ -2,5 +2,6 @@ import java.util.ArrayList;
 
 public class Student extends Person{
     private ArrayList<Course> coursesEnrolled = new ArrayList<Course>();
+    //Not even close to done yet.
 
 }//Student
