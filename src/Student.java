c47cc21160b96d45b71202db1fdcf13100444236
junew <junewi98@gmail.com>
2018-03-23 00:06:06 -0400

    Added currentResults

--- src/Student.java
+++ src/Student.java
@@ -3,5 +3,6 @@ import java.util.ArrayList;
 public class Student extends Person{
     private ArrayList<Course> coursesEnrolled = new ArrayList<Course>();
     //Not even close to done yet.
+    private ArrayList<String> currentResults = new ArrayList<String>();
 
 }//Student
