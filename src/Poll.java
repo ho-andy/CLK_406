63d5d9e058aad59191c86f12d18f0437223b85e6
Casey <casey.smith13@yahoo.ca>
2018-04-03 23:56:19 -0400

    added more stuff to poll and question

--- src/Poll.java
+++ src/Poll.java
@@ -1,2 +1,17 @@
-public class Poll {
+import java.util.ArrayList;
+
+public class Poll
+{
+    private String pollName;
+    private ArrayList<Question> poll = new ArrayList<Question>();
+
+    public Poll(String name)
+    {
+        this.pollName = name;
+    }
+
+    public void addQuestion(Question q)
+    {
+        this.poll.add(q);
+    }
 }
