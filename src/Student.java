b2c570a166c5ca714bcb8afb06925fdd010da02b
David <dszeto2313@gmail.com>
2018-04-04 00:11:40 -0400

    blah

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Student extends Person{
+    private String name, lastName;
     private ArrayList<Course> coursesEnrolled = new ArrayList<Course>();
     private ArrayList<String> currentResults = new ArrayList<String>();
 
@@ -8,8 +9,14 @@ public class Student extends Person{
         super(user, pass);
     }//Student
 
+    public Student()
+
     public boolean joinCourse(){
         return true;
+    }
+
+    public void editStudent(String name, String lastName) {
+        if()
     }
 
 
