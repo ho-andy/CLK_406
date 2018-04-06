bc327f8a402c28399d39ed3332054974277c7562
brian-lacdoo <brian.lacdoo@hotmail.com>
2018-04-05 21:21:19 -0400

    Extended Question class, Course class, Poll class

--- src/Question.java
+++ src/Question.java
@@ -3,7 +3,24 @@ import java.util.ArrayList;
 //
 public class Question
 {
-    private String string;
-    private int numOfQs;
-    private ArrayList<String> question = new ArrayList<String>();
+    private ArrayList<String> answers = new ArrayList<String>();
+    private String question;
+    private int correctAnswer;
+
+    public Question(String question, int correctAnswer){
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void addAnswer(){
+
+    }
+
+    public void deleteAnswer(){
+
+    }
+
+    public boolean checkAnswer(){
+        return true;
+    }
 }
