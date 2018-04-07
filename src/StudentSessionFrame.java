c401f35157d8cc09a41aad4f914d870098a8092c
Andy Ho <hoandy1998@gmail.com>
2018-04-06 20:58:31 -0400

    student session frame

--- src/StudentSessionFrame.java
+++ src/StudentSessionFrame.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionListener;
 
 public class StudentSessionFrame extends JFrame{
     private JButton backButton = new JButton("Back");
+    private JLabel questionLabel = new JLabel();
+    private JRadioButton answerButtons = new JRadioButton();
+    private JButton submitButton = new JButton("Submit");
+    private String selectedAnswer = "";
 
     private Person loggedIn;
 
@@ -14,6 +18,41 @@ public class StudentSessionFrame extends JFrame{
 
         this.add(backButton);
         backButton.addActionListener(new BackListener());
+
+        this.add(questionLabel);
+
+        questionLabel.setText("Wagwan fam?");
+
+        JRadioButton answer1 = new JRadioButton("nm fam");
+        JRadioButton answer2 = new JRadioButton("soft tings");
+        JRadioButton answer3 = new JRadioButton("yknow stuff");
+        JRadioButton answer4 = new JRadioButton("chilling with the boys");
+        JRadioButton answer5 = new JRadioButton("just chillin");
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(answer1);
+        buttonGroup.add(answer2);
+        buttonGroup.add(answer3);
+        buttonGroup.add(answer4);
+        buttonGroup.add(answer5);
+
+        answer1.addActionListener(new AnswerListener());
+        answer2.addActionListener(new AnswerListener());
+        answer3.addActionListener(new AnswerListener());
+        answer4.addActionListener(new AnswerListener());
+        answer5.addActionListener(new AnswerListener());
+
+        JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+        radioPanel.add(answer1);
+        radioPanel.add(answer2);
+        radioPanel.add(answer3);
+        radioPanel.add(answer4);
+        radioPanel.add(answer5);
+
+        this.add(radioPanel);
+
+        this.add(submitButton);
+        submitButton.addActionListener(new SubmitListener());
     }
 
     public StudentSessionFrame(Person person){
@@ -24,7 +63,41 @@ public class StudentSessionFrame extends JFrame{
         backButton.addActionListener(new BackListener());
 
         loggedIn = person;
-        System.out.println(loggedIn);
+
+        this.add(questionLabel);
+
+        questionLabel.setText("Wagwan fam?");
+
+        JRadioButton answer1 = new JRadioButton("nm fam");
+        JRadioButton answer2 = new JRadioButton("soft tings");
+        JRadioButton answer3 = new JRadioButton("yknow stuff");
+        JRadioButton answer4 = new JRadioButton("chilling with the boys");
+        JRadioButton answer5 = new JRadioButton("just chillin");
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(answer1);
+        buttonGroup.add(answer2);
+        buttonGroup.add(answer3);
+        buttonGroup.add(answer4);
+        buttonGroup.add(answer5);
+
+        answer1.addActionListener(new AnswerListener());
+        answer2.addActionListener(new AnswerListener());
+        answer3.addActionListener(new AnswerListener());
+        answer4.addActionListener(new AnswerListener());
+        answer5.addActionListener(new AnswerListener());
+
+        JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+        radioPanel.add(answer1);
+        radioPanel.add(answer2);
+        radioPanel.add(answer3);
+        radioPanel.add(answer4);
+        radioPanel.add(answer5);
+
+        this.add(radioPanel);
+
+        this.add(submitButton);
+        submitButton.addActionListener(new SubmitListener());
     }
 
     public void closePanel(){
@@ -40,6 +113,22 @@ public class StudentSessionFrame extends JFrame{
             studentCourseFrame.setTitle("Course List");
             studentCourseFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             closePanel();
+        }
+    }
+
+    class AnswerListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JRadioButton button = (JRadioButton) e.getSource();
+            selectedAnswer = button.getText();
+        }
+    }
+
+    class SubmitListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(selectedAnswer);
+            //submit here
         }
     }
 }
